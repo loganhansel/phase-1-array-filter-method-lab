14e0347df68a60ba219ec869669e9707baa0cf53
loganhansel <loganhansel23@gmail.com>
2022-05-12 18:16:15 -0400

    commit tree 5be40df56a08ba418340c85de135bbb419ab12b3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(array, string){
+    return array.filter(i => i.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(i => i[0].toUpperCase() === string[0].toUpperCase());
+}
+
+function matchName(array, string){
+    return array.filter(i => i.name === string);
+}
